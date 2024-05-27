8c5617919338a8ec0eeb55d76fa1b76c0482bc2b
Nate Spilman <nate.spilman@gmail.com>
2024-05-27 12:45:45 -0700

    sending my first email with resend. adding requirements.txt

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("We out here")
+import os
+import resend
+
+resend.api_key = os.environ["RESEND_API_KEY"]
+
+params: resend.Emails.SendParams = {
+    "from": "Nate <pioneer@natespilman.com>",
+    "to": ["nate.spilman@gmail.com"],
+    "subject": "Pioneer",
+    "html": "<strong>WE OUT HERE</strong>",
+}
+
+email = resend.Emails.send(params)
+print(email)
